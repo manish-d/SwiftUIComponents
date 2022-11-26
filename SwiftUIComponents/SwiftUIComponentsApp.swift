d7fb5e00990eb0ab2ae58f5976ec444b085b9c15
Manish Dhameja <manish.dhameja@onefootball.com>
2022-11-26 13:41:06 +0100

    Init

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Manish Dhameja on 26.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
